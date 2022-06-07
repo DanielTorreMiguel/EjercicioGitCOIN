e5b7767fce1df4bab41804046755f703ca33cc9a
Daniel Torre <daniel.torre.miguel@gmail.com>
2022-06-07 17:16:03 +0200

    Creada estructura para tests unitarios

---
+++ UnitTests.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : main.cpp
+// Author      : Daniel Torre
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Unit Tests 
+//============================================================================
+
+#include <iostream>
+#include <string>
+#include <limits>
+
+using namespace std;
+int main()
+{
+    cout << "Prueba";
+}
